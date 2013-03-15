cf79b6d6cbeeda335b6da52d839e341e530caf54
Mark J Stang <markjstang@gmail.com>
2013-03-15 07:06:51 -0600

    Added opencmis client jar to project.  Started experimenting with creating a connection to a server and getting repository info.

--- src/chem100/core.clj
+++ src/chem100/core.clj
@@ -1,6 +1,39 @@
 (ns chem100.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(import '(org.apache.chemistry.opencmis.client.api CmisObject Folder ItemIterable Repository Session SessionFactory))
+(import org.apache.chemistry.opencmis.client.runtime.SessionFactoryImpl)
+(import org.apache.chemistry.opencmis.commons.SessionParameter)
+(import org.apache.chemistry.opencmis.commons.enums.BindingType)
+
+;; need to create a session to connect to:
+;; http://localhost:8081/inmemory/atom
+;; user test, password test
+
+(def sessionFactory (SessionFactoryImpl/newInstance))
+(def parameter (new java.util.HashMap))
+
+(. parameter put "hello" "world")
+;(. parameter put SessionParameter/ATOMPUB_URL "http://repo.opencmis.org/inmemory/atom/")
+(. parameter put SessionParameter/ATOMPUB_URL "http://localhost:8081/inmemory/atom/")
+(. parameter put SessionParameter/BINDING_TYPE (. BindingType/ATOMPUB value))
+
+(defn get-repositories [parameter]
+  (. sessionFactory getRepositories parameter))
+
+(defn repository-id [parameter]
+  (. (first (get-repositories parameter)) getId))
+
+(defn create-session [parameter]
+  )
+;        Repository repository = repositories.get(0);
+;        parameter.put(SessionParameter.REPOSITORY_ID, repository.getId());
+;        Session session = sessionFactory.createSession(parameter)
+;        System.out.println("Got a connection to repository: " + repository.getName() + ", with id: " + repository.getId());
+;        // Get everything in the root folder and print the names of the objects
+;        Folder root = session.getRootFolder();
+;        ItemIterable<CmisObject> children = root.getChildren();
+;        System.out.println("Found the following objects in the root folder:-");
+;        for (CmisObject o : children) {
+;            System.out.println(o.getName());
+;        }
+
